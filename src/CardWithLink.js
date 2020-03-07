df23c6102816dc9d6df5f866c5f51fd464cc893b
YouMinTW <m791213p@gmail.com>
2020-03-07 20:24:52 +0800

    所以我不想要的div呢? 啊 搞定

--- src/CardWithLink.js
+++ src/CardWithLink.js
@@ -3,10 +3,10 @@ import React from 'react'
 const CardWithLink = () => {
   // 如果我不想要div 呢？ 試著刪除外層的div看看
   return (
-  <div>
+  <>
     <h2>How're you?</h2>
     <a href="https://www.google.com" target="_blank">Let's go to google!</a>
-   </div>
+   </>
   )
 }
 export default CardWithLink
