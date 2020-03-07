9b8aa6cd6d22731064fd5440f8c4e9760e6b6496
YouMinTW <m791213p@gmail.com>
2020-03-07 20:00:20 +0800

    Let's create Component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
 import React from 'react';
-import nameAnythingYouWant,{ sayNiHou as Nihou } from './sayHi.js'
-
-// 某個會被儲存在Global變數isJohn
-var isJohn = true
-let nameIsJohn = 'John'
-function changeName () {
-  if(isJohn) {
-    nameIsJohn = 'QQ'
-  }
-}
-function Card4() {
-  return <div>{nameIsJohn}</div> 
-}
-// 嘗試著解除下面這1行程式碼的註解
-// changeName()
-// 嘗試著解除下面這幾行程式碼的註解
-// Card4 = () => {
-//   if(isJohn === true)
-//   {
-//     nameIsJohn = 'Tom'
-//   }
-//   return <div>{nameIsJohn}</div>
-// }
-
+// 來寫個從外部引進的元件吧!
 function App() {
-  // nameAnythingYouWant他會直接指向有default的function
-  nameAnythingYouWant()
-  // 原本沒default的export出去之後，要引入需要{}，而透過 as 之後可以取別名
-  Nihou()
   return (
     <div className="App">
-      <Card/>
-      <Card2/>
-      <Card3 name={'Ken'}/>
-      <Card3 name={'KK'}/>
-      <Card4 />
     </div>
   );
 }
-
-function Card () {
-  return <div>123</div>
-}
-const Card2 = () => <div>Card2</div>
-const Card3 = ({name}) => <div>{`My name is ${name}`}</div>
 export default App;
