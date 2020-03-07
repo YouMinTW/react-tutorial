bec553c9211021ec8704c54bd396656c7a46cd9d
YouMinTW <m791213p@gmail.com>
2020-03-07 20:14:29 +0800

    Card with Link (如果我不想要div 呢？ 試著刪除外層的div看看)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from './Card'
+import CardWithLink from './CardWithLink';
 // 來寫個從外部引進的元件吧!
 // 再來寫個含有 h2 (how're you), a (let's google導去google)的元件吧
 
 function App() {
   return (
     <div className="App">
+      <CardWithLink />
       <Card name='ken'></Card>
       <Card name='Mayu'></Card>
     </div>
