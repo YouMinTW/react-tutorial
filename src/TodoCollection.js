b0f1348826b72f8e373d5883c29d23afc67bb39d
YouMinTW <m791213p@gmail.com>
2020-03-07 20:42:16 +0800

    來新增些Todo吧

--- src/TodoCollection.js
+++ src/TodoCollection.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 const TodoCollection = () => {
   const [ todos,setTodos ] = React.useState([{name:'刷牙',done:false},{name:'遛狗',done:false}])
+  const handleAddToDo = () => {
+    setTodos([...todos, {name:'睡覺',done:false}])
+  }
+  const handleAddToDoType2 = (newTodo) => {
+    setTodos([...todos, newTodo])
+  }
   return (
-    <ul>
-      { todos.map(todo => <li>{todo.name}</li>) }
-    </ul>
+    <>
+      <button onClick={handleAddToDo}>新增個todo吧</button>
+      <button onClick={()=>handleAddToDoType2({name:'睡覺',done:false})}>新增個todo Type2吧</button>
+      <ul>
+        { todos.map(todo => <li>{todo.name}</li>) }
+      </ul>
+    </>
   )
 }
 export default TodoCollection
