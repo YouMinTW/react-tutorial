92f5fed82e2adb4e6c6656a46b95729d2e466745
YouMinTW <m791213p@gmail.com>
2020-03-07 18:37:24 +0800

    add functional component and props
    functional 元件的兩種寫法
    以及帶入props
    Cards元件的重複使用性

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-
+      <Card/>
+      <Card2/>
+      <Card3 name={'Ken'}/>
+      <Card3 name={'KK'}/>
     </div>
   );
 }
 
+function Card () {
+  return <div>123</div>
+}
+const Card2 = () => <div>Card2</div>
+const Card3 = ({name}) => <div>{`My name is ${name}`}</div>
 export default App;
