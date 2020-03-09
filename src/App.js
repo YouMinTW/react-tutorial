196c37798dacbee3ae6ed15e2b0589d8e928c139
YouMinTW <m791213p@gmail.com>
2020-03-10 00:49:50 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoCollection from './TodoCollection.js';
+import Counter from './Counter.js';
 
 function App() {
 
   return (
     <div className="App">
+      <Counter />
       <TodoCollection />
     </div>
   );
