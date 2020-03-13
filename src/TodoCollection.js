d15da64eb597ffe1caedef739cd28796492f09c3
YouMinTW <m791213p@gmail.com>
2020-03-14 00:51:32 +0800

    稍微關注點分離一下吧
    你會看到console log還是有個警告，說名我們必須要有個key

--- src/TodoCollection.js
+++ src/TodoCollection.js
@@ -14,14 +14,16 @@ const TodoCollection = () => {
     }
   }
   return (
+    <>
     <form onSubmit={handleSubmit}>
       <input type='text' onChange={handleChange} value={newTodo}></input>
       <button onClick={handleSubmit}>新增個todo吧 另一種方式</button>
       <button type='submit'>新增個todo</button>
-      <ul>
-        { todos.map(todo => <li>{todo.name}</li>) }
-      </ul>
     </form>
+    <ul>
+        { todos.map(todo => <li>{todo.name}</li>) }
+    </ul>
+    </>
   )
 }
 export default TodoCollection
