461726031b5437e5fd0bd3ca7672fcfe6e838f43
YouMinTW <m791213p@gmail.com>
2020-03-14 01:10:16 +0800

    繼續useEffect 為什麼我們的console.log沒有跟著一起更新呢？

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import TodoCollection from './TodoCollection.js';
 import Counter from './Counter.js';
-
+import Title,{ Title1 } from './Title'
 function App() {
-
+const [title, setTitle] = React.useState('default title')
+const handleChange = (e) => {
+  setTitle(e.target.value)
+}
   return (
     <div className="App">
+      <input type="text" onChange={handleChange} value={title}></input>
+      <Title title={title}/>
+      {/* <Title1 title={title}/> */}
       <Counter />
       <TodoCollection />
     </div>
