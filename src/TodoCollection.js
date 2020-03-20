45e53e2ea4b2728547a2feae867ea5a730aa8977
YouMinTW <m791213p@gmail.com>
2020-03-21 02:45:28 +0800

    來個編輯todo吧

--- src/TodoCollection.js
+++ src/TodoCollection.js
@@ -3,6 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 const TodoCollection = () => {
   const [ todos,setTodos ] = React.useState([])
   const [ newTodo,setNewTodo ] = React.useState('')
+  const [ updateTodo,setUpdateTodo ] = React.useState({edit:false})
+  const [ restTodos,setRestTodos ] = React.useState([])
   const handleChange = (e) => {
     setNewTodo(e.currentTarget.value)
   }
@@ -15,24 +17,40 @@ const TodoCollection = () => {
   },[todos])
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(newTodo !== ''){
-      setTodos([...todos, {id:uuidv4(),name:newTodo, done:false}])
-      setNewTodo('')
+    if(newTodo !== '' && updateTodo.edit ===false) {
+      setTodos([...todos, {id:uuidv4(),name:newTodo, done:false, edit:false}])
+    } else if (updateTodo.edit ===true) {
+      setTodos([...restTodos, {id:updateTodo.id,name:newTodo, done:updateTodo.done, edit:false}])
     }
+    setNewTodo('')
+    setUpdateTodo({edit:false})
   }
   const handleDelete = (id) => {
     const filterTodos = todos.filter((todo) =>todo.id !== id)
     setTodos(filterTodos)
+    setNewTodo('')
+    setRestTodos([])
+    setUpdateTodo({edit:false})
   }
   const handleToggleFinish  = (id) =>{
     const filterTodos = todos.map((todo) => todo.id === id ? { ...todo, done:!todo.done} : todo)
     setTodos(filterTodos)
+    setNewTodo('')
+    setRestTodos([])
+    setUpdateTodo({edit:false})
+  }
+  const handleEdit  = (id) =>{
+    const selectedTodo = todos.find((todo) => todo.id === id)
+    const filterTodos = todos.filter((todo) => todo.id !== id)
+    setRestTodos(filterTodos)
+    setUpdateTodo({ ...selectedTodo, edit:true})
+    setNewTodo(selectedTodo.name)
   }
   return (
     <>
     <form onSubmit={handleSubmit}>
       <input type='text' onChange={handleChange} value={newTodo}></input>
-      <button type='submit'>新增個todo</button>
+      <button type='submit'>{updateTodo.edit ? '編輯todos':'新增個todo'}</button>
     </form>
 
     <ul>
@@ -41,8 +59,10 @@ const TodoCollection = () => {
             <h3> {todo.name}</h3>
             <span>todo id: {todo.id}</span>
             <h6> todo is finished or not? <mark>{todo.done? 'yes' : 'no'}</mark></h6>
+            <h6> todo edit? <mark>{todo.edit? 'yes' : 'no'}</mark></h6>
             <button onClick={() =>handleDelete(todo.id)}>delete Todo</button>
             <button onClick={() =>handleToggleFinish(todo.id)}>finish Todo</button>
+            <button onClick={() =>handleEdit(todo.id)}>edit Todo</button>
           </li>
         )
         ) }
