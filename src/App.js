0100eab82a50e490aae2d323ae36f59e224a8667
YouMinTW <m791213p@gmail.com>
2020-03-28 15:10:50 +0800

    先講講Link吧！ 讓我們不用再手動從網址列輸入網址了
    我們可以透過點擊頁面的連結切換頁面

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ function App() {
   return (
     <Router>
       <div  className="App"> 
+
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
           <Route path="/">
             <Home />
           </Route>
