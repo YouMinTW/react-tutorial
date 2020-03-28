786adaf31ccab7c933260b22a005fc7b71fa1486
YouMinTW <m791213p@gmail.com>
2020-03-28 15:02:45 +0800

    Router?? 嘗試著在url輸入 /, /about, /dashboard看看吧？

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import TodoCollection from './TodoCollection.js';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App">
-      <TodoCollection />
+    <Router>
+      <div  className="App"> 
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+      </div>
+    </Router>
+  );
+}
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
     </div>
   );
 }
